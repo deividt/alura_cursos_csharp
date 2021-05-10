546e5dd6a0d6ed98c4173e53fd12165474b54862
Deividt Gemeli <deividt@gmail.com>
2021-05-10 16:32:03 -0300

    TDD - Projeto console para teste

---
+++ curso_tdd/Alura.LeilaoOnline/Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Alura.LeilaoOnline.Core;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Inicia leilão
+            Leilao leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            // Lances
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+
+            // Lance errado
+            leilao.RecebeLance(maria, 990);
+
+            // Finaliza pregão
+            leilao.TerminaPregao();
+
+            // Resultado
+            Console.WriteLine(leilao.Ganhador.Valor);
+        }
+    }
+}
