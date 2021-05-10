3dfdf1c8e111a6a93cc99578eb2756e730ea0e99
Deividt Gemeli <deividt@gmail.com>
2021-05-10 16:20:41 -0300

    TDD - Implementado ganhador do leilão

--- curso_tdd/Alura.LeilaoOnline/Alura.LeilaoOnline.Core/Leilao.cs
+++ curso_tdd/Alura.LeilaoOnline/Alura.LeilaoOnline.Core/Leilao.cs
@@ -1,19 +1,31 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Alura.LeilaoOnline.Core
 {
     public class Leilao
     {
+        #region Campos
         private IList<Lance> _lances;
+        #endregion
+
+        #region Propriedades
         public IEnumerable<Lance> Lances => _lances;
+
         public string Peca { get; }
 
+        public Lance Ganhador { get; private set; }
+        #endregion
+
+        #region Construtor
         public Leilao(string peca)
         {
             Peca = peca;
             _lances = new List<Lance>();
         }
+        #endregion
 
+        #region Métodos
         public void RecebeLance(Interessada cliente, double valor)
         {
             _lances.Add(new Lance(cliente, valor));
@@ -26,7 +38,9 @@ namespace Alura.LeilaoOnline.Core
 
         public void TerminaPregao()
         {
-
+            // Ganhador é o último lance
+            this.Ganhador = Lances.Last();
         }
+        #endregion
     }
 }
