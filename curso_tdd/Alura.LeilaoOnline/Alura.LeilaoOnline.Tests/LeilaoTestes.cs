9382d54969ac75ba695091bbe3a837cf3f85dccd
Deividt Gemeli <deividt@gmail.com>
2021-05-11 13:12:20 -0300

    TDD - Adicionado testes

--- curso_tdd/Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
+++ curso_tdd/Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -6,6 +6,53 @@ namespace Alura.LeilaoOnline.Tests
     public class LeilaoTestes
     {
         [Fact]
+        public void LeilaoComTresClientes()
+        {
+            // Arranje
+            Leilao leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+            var beltrano = new Interessada("Beltrano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 990);
+            leilao.RecebeLance(maria, 1000);
+            leilao.RecebeLance(beltrano, 1400);
+
+            // Act
+            leilao.TerminaPregao();
+
+            // Assert
+            double valorEsperado = 1400;
+            double valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+            Assert.Equal(beltrano, leilao.Ganhador.Cliente);
+        }
+
+        [Fact]
+        public void LeilaoComLancesOrdenadosPorValor()
+        {
+            // Arranje
+            Leilao leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 990);
+            leilao.RecebeLance(maria, 1000);
+
+            // Act
+            leilao.TerminaPregao();
+
+            // Assert
+            double valorEsperado = 1000;
+            double valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
         public void LeilaoComVariosLances()
         {
             // Arranje
@@ -16,13 +63,13 @@ namespace Alura.LeilaoOnline.Tests
             leilao.RecebeLance(fulano, 800);
             leilao.RecebeLance(maria, 900);
             leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(maria, 1200);
+            leilao.RecebeLance(maria, 990);
 
             // Act
             leilao.TerminaPregao();
 
             // Assert
-            double valorEsperado = 1200;
+            double valorEsperado = 1000;
             double valorObtido = leilao.Ganhador.Valor;
             Assert.Equal(valorEsperado, valorObtido);
         }
