65ae274f1dff6bd25097bafde72866786114f406
Deividt Gemeli <deividt@gmail.com>
2021-05-10 23:08:06 -0300

    TDD - Projeto de testes com xUnit

---
+++ curso_tdd/Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -0,0 +1,48 @@
+﻿using Alura.LeilaoOnline.Core;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoTestes
+    {
+        [Fact]
+        public void LeilaoComVariosLances()
+        {
+            // Arranje
+            Leilao leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 1200);
+
+            // Act
+            leilao.TerminaPregao();
+
+            // Assert
+            double valorEsperado = 1200;
+            double valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        public void LeilaoComApenasUmLance()
+        {
+            // Arranje
+            Leilao leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            // Act
+            leilao.TerminaPregao();
+
+            // Assert
+            double valorEsperado = 800;
+            double valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
