d4dd061aa55740016408be937855e3d496da72d0
Deividt Gemeli <deividt@gmail.com>
2021-05-11 13:57:08 -0300

    TDD - Teste com anotação [Theory]

--- curso_tdd/Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
+++ curso_tdd/Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -5,75 +5,26 @@ namespace Alura.LeilaoOnline.Tests
 {
     public class LeilaoTestes
     {
-        [Fact]
-        public void LeilaoComTresClientes()
-        {
-            // Arranje - cenário
-            // Dado leilão com 3 clientes e lances realizados por eles
-            Leilao leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
-            var beltrano = new Interessada("Beltrano", leilao);
 
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(fulano, 990);
-            leilao.RecebeLance(maria, 1000);
-            leilao.RecebeLance(beltrano, 1400);
-
-            // Act - método sob teste
-            // Quando o pregão/leilão termina
-            leilao.TerminaPregao();
-
-            // Assert
-            // Então o valor esperado é o maior valor dado
-            // e o cliente ganhador é o que deu o maior lance
-            double valorEsperado = 1400;
-            double valorObtido = leilao.Ganhador.Valor;
-            Assert.Equal(valorEsperado, valorObtido);
-            Assert.Equal(beltrano, leilao.Ganhador.Cliente);
-        }
-
-        [Fact]
-        public void LeilaoComLancesOrdenadosPorValor()
+        [Theory]
+        [InlineData(1200, new double[] { 800, 900, 1000, 1200 })]
+        [InlineData(1000, new double[] { 800, 900, 1000, 990 })]
+        [InlineData(800, new double[] { 800 })]
+        public void LeilaoComVariosLances(double valorEsperado, double[] ofertas)
         {
             // Arranje
             Leilao leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
 
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(fulano, 990);
-            leilao.RecebeLance(maria, 1000);
+            foreach (double valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
 
             // Act
             leilao.TerminaPregao();
 
             // Assert
-            double valorEsperado = 1000;
-            double valorObtido = leilao.Ganhador.Valor;
-            Assert.Equal(valorEsperado, valorObtido);
-        }
-
-        [Fact]
-        public void LeilaoComVariosLances()
-        {
-            // Arranje
-            Leilao leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(maria, 990);
-
-            // Act
-            leilao.TerminaPregao();
-
-            // Assert
-            double valorEsperado = 1000;
             double valorObtido = leilao.Ganhador.Valor;
             Assert.Equal(valorEsperado, valorObtido);
         }
@@ -89,24 +40,6 @@ namespace Alura.LeilaoOnline.Tests
 
             // Assert
             double valorEsperado = 0;
-            double valorObtido = leilao.Ganhador.Valor;
-            Assert.Equal(valorEsperado, valorObtido);
-        }
-
-        [Fact]
-        public void LeilaoComApenasUmLance()
-        {
-            // Arranje
-            Leilao leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-
-            // Act
-            leilao.TerminaPregao();
-
-            // Assert
-            double valorEsperado = 800;
             double valorObtido = leilao.Ganhador.Valor;
             Assert.Equal(valorEsperado, valorObtido);
         }
