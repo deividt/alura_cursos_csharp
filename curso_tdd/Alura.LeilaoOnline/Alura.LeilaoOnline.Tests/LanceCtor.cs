ab863d6de1b2261864965823f34924322da98b61
Deividt Gemeli <deividt@gmail.com>
2021-05-14 13:15:07 -0300

    TDD - 04.05 - Testando outra exceção

---
+++ curso_tdd/Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LanceCtor.cs
@@ -0,0 +1,20 @@
+﻿namespace Alura.LeilaoOnline.Tests
+{
+    using Alura.LeilaoOnline.Core;
+    using Xunit;
+
+    public class LanceCtor
+    {
+        [Fact]
+        public void LancaArgumentExceptionDadoValorNegativo()
+        {
+            // Arrange
+            var valorNegativo = -100;
+
+            // Assert
+            Assert.Throws<System.ArgumentException>(
+                // Act
+                () => new Lance(null, valorNegativo));
+        }
+    }
+}
