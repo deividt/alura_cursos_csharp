f0e278bd2b715753257b086714f551825a2973bc
Deividt <deividt.gemeli@arctouch.com>
2021-07-13 16:58:09 -0300

    Design Patterns - 03.01 - Template Method

---
+++ curso_design_patterns/CursoDesignPatterns/CursoDesignPatterns/IKCV.cs
@@ -0,0 +1,34 @@
+namespace CursoDesignPatterns
+{
+    public class IKCV:TemplateDeImpostoCondicional
+    {
+        public override bool DeveUsarMaximaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor > 500 && temItemMaiorQue100ReaisNo(orcamento);
+        }
+
+        public override double MaximaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.01;
+        }
+       
+        public override double MinimaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.06;
+        }
+        
+
+        private bool temItemMaiorQue100ReaisNo(Orcamento orcamento)
+        {
+            foreach (var item in orcamento.Itens)
+            {
+                if (item.Valor > 100)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
