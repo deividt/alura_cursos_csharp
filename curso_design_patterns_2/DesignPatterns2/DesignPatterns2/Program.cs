a71d38ea897a716296ab120a4d5d9abf0300e5b4
Deividt gemeli <deividt@gmail.com>
2021-09-27 15:56:16 -0300

    Design Patterns 2 - 04.01 - Interpreter

--- curso_design_patterns_2/DesignPatterns2/DesignPatterns2/Program.cs
+++ curso_design_patterns_2/DesignPatterns2/DesignPatterns2/Program.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq.Expressions;
 using DesignPatterns2.Cap1;
 using DesignPatterns2.Cap2;
 using DesignPatterns2.Cap3;
+using DesignPatterns2.Cap4;
 
 namespace DesignPatterns2
 {
@@ -16,10 +18,25 @@ namespace DesignPatterns2
 
             // FlyweightPattern();
 
-            MementoPattern();
+            //MementoPattern();
+
+            Interpreter();
 
             Console.WriteLine("Press any key to exit!");
             Console.ReadKey();
+        }
+
+        private static void Interpreter()
+        {
+            // ((1 + 100) + 10) + (20 - 10)
+            IExpressao esquerda = new Soma(new Soma( new Numero(1), new Numero(100)), new Numero(10));
+            IExpressao direita = new Subtracao(new Numero(20), new Numero(10));
+            IExpressao soma = new Soma(esquerda, direita);
+            Console.WriteLine(soma.Avalia());
+
+            Expression exSoma = Expression.Add(Expression.Constant(10), Expression.Constant(100));
+            Func<int> funcao = Expression.Lambda<Func<int>>(exSoma).Compile();
+            Console.WriteLine(funcao());
         }
 
         private static void FactoryPattern()
