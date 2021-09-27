1a87db8ccb736487b87b34f7a1d0591443af7bef
Deividt gemeli <deividt@gmail.com>
2021-09-27 16:28:12 -0300

    Design Patterns 2 - 05.01 - Visitor

--- curso_design_patterns_2/DesignPatterns2/DesignPatterns2/Cap4/IExpressao.cs
+++ curso_design_patterns_2/DesignPatterns2/DesignPatterns2/Cap4/IExpressao.cs
@@ -1,7 +1,11 @@
+using DesignPatterns2.Cap5;
+
 namespace DesignPatterns2.Cap4
 {
     public interface IExpressao
     {
         int Avalia();
+
+        void Aceita(IVisitor impressoraVisitor);
     }
 }
