066c7cb637b591e383b156c3a318900e0ad5ee75
Deividt Gemeli <deividt@gmail.com>
2021-09-27 21:35:11 -0300

    Design Patterns 2 - 08.01 - Adapter (#2)

---
+++ curso_design_patterns_2/DesignPatterns2/DesignPatterns2/Cap8/Cliente.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DesignPatterns2.Cap8
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+    }
+}
