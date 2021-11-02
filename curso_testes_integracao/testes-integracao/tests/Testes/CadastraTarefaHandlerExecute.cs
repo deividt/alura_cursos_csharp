1c00929bfc69393f2069acc1db7f7b78cf7772c3
Deividt gemeli <deividt@gmail.com>
2021-11-01 22:39:38 -0300

    Testes de Integração - 02.02 - InMemoryDatabase

--- curso_testes_integracao/testes-integracao/tests/Testes/CadastraTarefaHandlerExecute.cs
+++ curso_testes_integracao/testes-integracao/tests/Testes/CadastraTarefaHandlerExecute.cs
@@ -4,6 +4,7 @@ using Alura.CoisasAFazer.Core.Commands;
 using Alura.CoisasAFazer.Core.Models;
 using Alura.CoisasAFazer.Infrastructure;
 using Alura.CoisasAFazer.Services.Handlers;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Testes
@@ -16,7 +17,11 @@ namespace Testes
             // Arrange
             var comando = new CadastraTarefa("Estudar xUnit", new Categoria("Estudo"), new DateTime(2019, 12, 31));
 
-            var repositorio = new RepositorioFake();
+            var options = new DbContextOptionsBuilder<DbTarefasContext>()
+                .UseInMemoryDatabase("DbTarefasContext")
+                .Options;
+            var contexto = new DbTarefasContext(options);
+            var repositorio = new RepositorioTarefa(contexto);
             
             var handler = new CadastraTarefaHandler(repositorio);
 
