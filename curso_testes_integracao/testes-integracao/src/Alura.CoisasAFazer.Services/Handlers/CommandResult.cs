ea5e38d619b81b1e32da27991f46762511fa8097
Deividt gemeli <deividt@gmail.com>
2021-11-02 22:21:15 -0300

    Testes de Integração - 03.02-03 - Lançando Exceções

---
+++ curso_testes_integracao/testes-integracao/src/Alura.CoisasAFazer.Services/Handlers/CommandResult.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Alura.CoisasAFazer.Services.Handlers
+{
+    public class CommandResult
+    {
+        public bool IsSuccess { get; }
+
+        public CommandResult(bool success)
+        {
+            IsSuccess = success;
+        }
+    }
+}
--- curso_testes_integracao/testes-integracao/tests/Testes/CadastraTarefaHandlerExecute.cs
+++ curso_testes_integracao/testes-integracao/tests/Testes/CadastraTarefaHandlerExecute.cs
@@ -5,6 +5,7 @@ using Alura.CoisasAFazer.Core.Models;
 using Alura.CoisasAFazer.Infrastructure;
 using Alura.CoisasAFazer.Services.Handlers;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using Xunit;
 
 namespace Testes
@@ -31,6 +32,26 @@ namespace Testes
             // Assert
             var tarefa = repositorio.ObtemTarefas(t => t.Titulo == "Estudar xUnit").FirstOrDefault();
             Assert.NotNull(tarefa);
+        }
+
+        [Fact]
+        public void QuandoExceptionForLancadaResultadoIsSuccessDeveserFalso()
+        {
+            // Arrange
+            var comando = new CadastraTarefa("Estudar xUnit", new Categoria("Estudo"), new DateTime(2019, 12, 31));
+
+            var mock = new Mock<IRepositorioTarefas>();
+            mock.Setup(r => r.IncluirTarefas(It.IsAny<Tarefa[]>())).
+                Throws(new Exception("Houve um erro na inclusão de tarefas"));
+            var repo = mock.Object;
+            
+            var handler = new CadastraTarefaHandler(repo);
+
+            // Act
+            CommandResult resultado = handler.Execute(comando);
+
+            // Assert
+            Assert.False(resultado.IsSuccess);
         }
     }
 }
