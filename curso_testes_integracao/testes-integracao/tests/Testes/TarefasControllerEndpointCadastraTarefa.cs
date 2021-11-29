f6793311866466bfa3bc5fcf876cf013c54327ed
Deividt gemeli <deividt@gmail.com>
2021-11-29 14:33:54 -0300

    Testes de Integração - 06.03 - Aplicando em controladores AspNet Core

--- curso_testes_integracao/testes-integracao/tests/Testes/TarefasControllerEndpointCadastraTarefa.cs
+++ curso_testes_integracao/testes-integracao/tests/Testes/TarefasControllerEndpointCadastraTarefa.cs
@@ -25,7 +25,6 @@ namespace Testes
               .Options;
           var contexto = new DbTarefasContext(options);
           contexto.Categorias.Add(new Categoria(20, "Estudo"));
-          contexto.SaveChanges();
           
           var repo = new RepositorioTarefa(contexto);
 
@@ -40,6 +39,33 @@ namespace Testes
 
           // Assert
           Assert.IsType<OkResult>(retorno); // 200
+        }
+
+        [Fact]
+        public void QuandoExcecaoForLancadaDeveRetornarStatusCode500()
+        {
+            // Arrange
+            var mockLogger = new Mock<ILogger<CadastraTarefaHandler>>();
+
+            var mock = new Mock<IRepositorioTarefas>();
+            mock.Setup(r => r.ObtemCategoriaPorId(20)).Returns(new Categoria(20, "Estudo"));
+            mock.Setup(r => r.IncluirTarefas(It.IsAny<Tarefa[]>())).Throws(new Exception("Houve um erro"));
+
+            var repo = mock.Object;
+
+            var controlador = new TarefasController(repo, mockLogger.Object);
+            var model = new CadastraTarefaVM();
+            model.IdCategoria = 20;
+            model.Titulo = "Estudar xUnit";
+            model.Prazo = new DateTime(2021, 12, 31);
+          
+            // Act
+            var retorno = controlador.EndpointCadastraTarefa(model);
+
+            // Assert
+            Assert.IsType<StatusCodeResult>(retorno);
+            var statusCodeRetornado = (retorno as StatusCodeResult).StatusCode;
+            Assert.Equal(500, statusCodeRetornado);
         }
     }
 }
